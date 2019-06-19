16c2cc000b1bbc454b5fd9a66324b48823e3e3fc
DotingPrawn <shinakuma3000@hotmail.com>
2019-06-19 16:07:23 -0300

    commit dois

--- teste.py
+++ teste.py
@@ -1,3 +1,14 @@
 import simpy
 
-print('git teste')
+def lista_elementos ():
+    i = 0
+    yield i
+    i = 1
+    yield i
+    i = 2
+    yield i
+
+l = lista_elementos()
+print(next(l))
+print(next(l))
+print(next(l))

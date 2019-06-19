16671da4e79b7347bb17fe49d7bea6d6ea4acc9f
DotingPrawn <shinakuma3000@hotmail.com>
2019-06-19 16:17:43 -0300

    teste1

---
+++ ExempleSimpy.py
@@ -0,0 +1,16 @@
+import simpy
+
+def Pessoa():
+    def __init__(self,env):
+        self.env = env
+        self.env.process(self.run())
+    
+    def run(self):
+        while True:
+            print('Começou a estudar em: ', self.env.now)
+            tempEstudo = 2
+            yield self.env.timeout(tempEstudo)
+
+            print('Iniciou as redes sociais em: ',self.env.now)
+            tempRedes = 5
+            yield self.env.timeout(tempRedes)
